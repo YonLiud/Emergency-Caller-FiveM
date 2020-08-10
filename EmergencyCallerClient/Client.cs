c075f3c34a323d1a8a67750022e3b2380bef4074
Yon Liud <45634836+YonLiud@users.noreply.github.com>
2020-08-10 14:46:09 +0300

    First Commit

---
+++ EmergencyCallerClient/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using EmergencyCallerServer;
+using static CitizenFX.Core.Native.API;
+using srv = EmergencyCallerServer.Server;
+
+
+namespace EmergencyCallerClient
+{
+    public class Client : BaseScript
+    {
+        public Client()
+        {
+            EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
+            EventHandlers["outcoming911"] += new Action<string>(OnRecieve911);
+        }
+
+        private void OnClientResourceStart(string resourceName)
+        {
+
+            Debug.WriteLine("Emergency Caller has Booted up");
+            if (GetCurrentResourceName() != resourceName) return;
+
+            RegisterCommand("911", new Action<int, List<object>, string>((source, args, raw) =>
+            {
+                string name = Game.Player.Name;
+                Vector3 Location = Game.PlayerPed.Position;
+                TriggerServerEvent("incoming911", Game.Player.Name) ;
+            }), false);
+        }
+        private void OnRecieve911(string message)
+        {
+            TriggerEvent("chat:addMessage", new
+            {
+                color = new[] { 0, 204, 204 },
+                multiline = true,
+                args = new[] { "911", message }
+            });
+        }
+    }
+}
---
+++ EmergencyCallerServer/Server.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace EmergencyCallerServer
+{
+    public class Server : BaseScript
+    {
+        public Server()
+        {
+            EventHandlers["incoming911"] += new Action<string>(OnCall);
+        }
+        public void OnResourceStart(string resourceName)
+        {
+            if (GetCurrentResourceName() != resourceName) return;
+        }
+
+        public void OnCall(string name)
+        {
+            string message = (name + " has dialed 911!");
+            TriggerClientEvent("outcoming911", message);
+        }
+
+    }
+}
